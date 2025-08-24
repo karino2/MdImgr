43f43aba5449195dacb0c1fd58f2e31d5a343fc5
Kazuma Arino <hogeika2@gmail.com>
2025-08-24 14:48:58 +0900

    Support history.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -7,21 +7,160 @@ import deleteIcon from './assets/delete.svg'
 
 const TARGET_DIR_KEY = 'targetDir'
 const TEMPLATE_KEY = 'template'
+const HISTORY_KEY = 'history'
 const INITIAL_TEMPLATE = '![images/SomeDir/$1]("images/SomeDir/$1")'
+const MAX_HISTORY = 20
 
 const templateInput = document.getElementById('template-input')
 
 templateInput.addEventListener('input', async (event) => {
     const template = event.target.value
     localStorage.setItem(TEMPLATE_KEY, template)
+    maySaveHistory({dir: localStorage.getItem(TARGET_DIR_KEY), template: template})
     await SetTemplate(template)
 });
 
+async function SetNewTemplate(template) {
+    templateInput.value = template
+    localStorage.setItem(TEMPLATE_KEY, template)
+    await SetTemplate(template)
+}
+
+
 document.getElementById('reset-template-button').addEventListener('click', async () => {
-    templateInput.value = INITIAL_TEMPLATE
-    localStorage.setItem(TEMPLATE_KEY, INITIAL_TEMPLATE)
-    await SetTemplate(INITIAL_TEMPLATE)
+    await SetNewTemplate(INITIAL_TEMPLATE)
+    maySaveHistory({dir: localStorage.getItem(TARGET_DIR_KEY), template: templateInput.value})
 });
+
+
+
+/**
+ * @typedef HistoryItem
+ * @type {object}
+ * @property {string} dir
+ * @property {string} template
+ */
+
+/**
+ * @returns {HistoryItem[]}
+ */
+function loadHistory() {
+    let json = localStorage.getItem(HISTORY_KEY)
+    if (json === null)
+        return []
+    return JSON.parse(json)
+}
+
+/**
+ * @param {HistoryItem[]} historyList
+ */
+function storeHistory(historyList) {
+    let json = JSON.stringify(historyList)
+    localStorage.setItem(HISTORY_KEY, json)
+}
+
+/**
+ * @param {HistoryItem[]} historyList
+ * @param {HistoryItem} newItem
+ * @returns {HistoryItem[]}
+ */
+function updateHistoryList(historyList, newItem) {
+    let newList = historyList.filter(
+        item=> item.dir != newItem.dir
+    )
+    if (newList.length > MAX_HISTORY)
+        newList = newList.slice(0, MAX_HISTORY)
+    
+    newList.unshift(newItem)
+    return newList
+}
+
+/**
+ * @type {HistoryItem[]}
+ */
+let g_historyList = []
+
+/**
+ * @type {HistoryItem}
+ */
+let g_lastHistoryItem = null
+
+
+/**
+ * @param {HistoryItem} newItem
+ */
+function maySaveHistory(newItem) {
+    if (g_lastHistoryItem === null || 
+        (g_lastHistoryItem.dir != newItem.dir || g_lastHistoryItem.template != newItem.template)) {
+        g_historyList = updateHistoryList(g_historyList, newItem)
+        storeHistory(g_historyList)
+
+        g_lastHistoryItem = newItem
+    }
+}
+
+/**
+ * @type {HTMLSelectElement}
+ */
+const historySelect = document.getElementById('history-dialog-select')
+
+const historyDialog = document.getElementById('history-dialog')
+
+
+/**
+ * @param {HistoryItem[]} historyList
+ */
+function populateHistory(historyList) {
+    historySelect.innerHTML = ''
+    if (historyList.length == 0) {
+        showToast("No history")
+        return false
+    }
+  historyList.forEach( (history, index) => {
+    const option = document.createElement('option')
+    const dir = history.dir
+    option.value = dir
+    option.text = dir.split('/').pop() || dir
+    option.dataset.index = index
+    historySelect.add(option)
+  })
+  return true
+}
+
+function showHistoryDialog() {
+    historyDialog.style.display = 'block'
+}
+
+function hideHistoryDialog() {
+    historyDialog.style.display = 'none'
+}
+
+document.getElementById('history-button').addEventListener('click', () => {
+    if (!populateHistory(g_historyList))
+        return 
+
+    showHistoryDialog()
+})
+
+document.getElementById('history-dialog-cancel-button').addEventListener('click', () => {
+    hideHistoryDialog()
+})
+
+document.getElementById('history-dialog-ok-button').addEventListener('click', async () => {
+  const selectedPath = historySelect.value;
+  if (selectedPath) {
+    const selectedOption = historySelect.options[historySelect.selectedIndex]
+    
+    const index = selectedOption.dataset.index
+
+    const cur = g_historyList[index]
+    await SetNewTemplate(cur.template)
+    await setNewDir(cur.dir)
+  }
+  hideHistoryDialog()
+})
+
+
 
 async function initializeApp() {
     let template = localStorage.getItem(TEMPLATE_KEY)
@@ -32,11 +171,14 @@ async function initializeApp() {
     templateInput.value = template
     await SetTemplate(template)
 
+    g_historyList = loadHistory()
+
     const storedDir = localStorage.getItem(TARGET_DIR_KEY)
     if (storedDir === null) {
         showToast("Please select target dir")
         await SelectDirAndNotify()
     } else {
+        g_lastHistoryItem = {dir: storedDir, template: template}
         await SetTargetDir(storedDir)
     }
 
@@ -94,6 +236,7 @@ runtime.EventsOn("show-toast", (msg) => {
 async function setNewDir(dir) {
     if (dir) {
         localStorage.setItem(TARGET_DIR_KEY, dir)
+        maySaveHistory({dir: dir, template: templateInput.value})
         await SetTargetDir(dir)
     }
 }
